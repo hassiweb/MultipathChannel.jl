a8bc616b0042d2407163de9e19db2a56a92e998f
Documenter.jl <documenter@juliadocs.github.io>
2020-08-29 17:28:50 +0000

    build based on 52a8077

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MultipathChannel","category":"page"},{"location":"#MultipathChannel","page":"Home","title":"MultipathChannel","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MultipathChannel]","category":"page"}]
+}
